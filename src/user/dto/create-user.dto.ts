bd7341fc1b9523e17d87b217d2302456223d4928
Donato Calvillo <donato.calvillo@3mium.com>
2023-04-17 22:08:21 -0600

    feat: add CreateUserDTO funtional

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,23 @@
-import { isUUID } from 'class-validator';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
-export class CreateUserDto {}
+export class CreateUserDto {
+  @IsString({ message: 'The fisrt name most be a string' })
+  @MinLength(2)
+  firstName: string;
+
+  @IsString({ message: 'The last name most be a string' })
+  @MinLength(2)
+  lastName: string;
+
+  @IsString({ message: 'The email most be a string' })
+  @IsEmail()
+  email: string;
+
+  @IsString({ message: 'The password most be a string' })
+  @MinLength(6)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password most have special characters, numbers and upper case',
+  })
+  password: string;
+}
